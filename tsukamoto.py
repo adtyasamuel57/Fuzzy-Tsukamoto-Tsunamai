9249176af0261f6e4dadf46d601ed64345591855
tamer sammy <tamersammy@yahoo.com>
2021-08-04 16:55:08 +0700

    update

--- tsukamoto.py
+++ tsukamoto.py
@@ -502,7 +502,7 @@ def show():
     if statusAman != 0:
         msg = "Aman ({})".format("%.2f" % statusAman)
         print('Status = ', msg)
-        respon = "Aman"
+        respon = "Bahaya"
     elif statusAwas != 0:
         msg = "Awas ({})".format("%.2f" % statusAwas)
         print('Status = ', msg)
@@ -510,7 +510,7 @@ def show():
     elif statusBahaya != 0:
         msg = "Bahaya ({})".format("%.2f" % statusBahaya)
         print('Status = ', msg)
-        respon = "Bahaya"
+        respon = "Aman"
     # hasil = respon,tggg,tg,spd,ka,gmp,kg
     # y = pd.DataFrame(hasil)
     # z = y.to_json()
